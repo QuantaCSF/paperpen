d35561f334ad2e11a75bda9baee25a8502ab73cc
--replace-all <--replace-all>
2020-05-03 22:33:21 -0500

    test

--- settings.js
+++ settings.js
@@ -1,12 +1,11 @@
 const QPP_ADMINS = [
     'eduardo@ctrlalttec.hackclub.com', //remove     
-
     'a01027016@itesm.mx', 
     'a01025326@itesm.mx',
     'a01027174@itesm.mx',
     'a01025344@itesm.mx', 
+    'a01025343@itesm.mx', 
     'a01025320@itesm.mx', 
-    'ana.paula.chapa.7@gmail.com',
     'negv@princeton.edu'
 ]
 
