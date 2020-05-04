b12754759e7a6d37b588ed23b72c0da7a8d827d1
--replace-all <--replace-all>
2020-05-03 21:19:44 -0500

    Testing

--- settings.js
+++ settings.js
@@ -1,7 +1,6 @@
 const QPP_ADMINS = [
     'eduardo@ctrlalttec.hackclub.com', //remove     
 
-    'a01025025@itesm.mx', //remvoe
     'a01027016@itesm.mx', 
     'a01025326@itesm.mx',
     'a01027174@itesm.mx',
@@ -12,6 +11,7 @@ const QPP_ADMINS = [
 ]
 
 const QPP_REVIEWERS = [
+    'a01025025@itesm.mx', //remvoe
 
     'aguzmantinajero@tec.mx',
     'ada.godinez@tec.mx',
