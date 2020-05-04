eaf04ba0d29079b0e63f7fe3c8ac55a58ade5131
Ricardo Skewes <rskeweszorrilla@gmail.com>
2020-05-04 01:45:24 -0500

    Update grading criteria -> more specific.

--- settings.js
+++ settings.js
@@ -31,10 +31,10 @@ const QPP_CRITERIA = [
     }, 
     {
         criteria: 'Implementación de elementos científicos veraces',
-        'bad': 'No se implementó ninguno de los elementos de ciencia indicados.',
-        'normal': 'No se observa investigación o conocimiento de los temas expuestos y éstos no son correspondientes con la realidad',
-        'good': 'Se nota investigación y conocimiento superficial de los conceptos científicos aunque existen algunas discordancias con la realidad.',
-        'excellent': 'Se nota una investigación y conocimiento profundos de los temas científicos expuestos. Los conceptos científicos interactúan de manera perfecta con los elementos ficticios. Los conceptos científicos se exponen de tal manera que son entendibles para una audiencia con conocimiento general.'
+        'bad': 'No se implementó ninguno de los elementos de ciencia estipulados en la Guía de Temas. Es imposible identificar la investigación realizada por el autor, previo a escribir el cuento.',
+        'normal': 'Se observa investigación o conocimiento superficial de los temas expuestos. El tema (o temas) elegido(s) casi no está(n) presente(s) en la historia. Tema e ideas principales salen del rango abordado por esta convocatoria.',
+        'good': 'Se nota investigación y conocimiento suficientes de los conceptos científicos. Los elementos científicos existen, pero no están bien integrados.',
+        'excellent': 'Se nota una investigación y conocimiento profundos de los temas científicos expuestos. Los conceptos científicos interactúan de manera perfecta con los elementos ficticios. Los conceptos científicos se exponen de tal manera que son comprensibles para una audiencia con conocimiento general.'
     }, 
     {
         criteria: 'Elementos literarios',
@@ -47,15 +47,15 @@ const QPP_CRITERIA = [
         criteria: 'Redacción',
         'bad': 'La redacción es pobre, confusa y difícil de entender, sin implementación alguna de elementos lingüísticos y con faltas de ortografía y redacción.',
         'normal': 'La redacción cuenta con faltas de ortografía y redacción y hay deficiencia de elementos lingüísticos.',
-        'good': 'Se observa una buena redacción con buen uso de elementos lingüísticos.',
-        'excellent': 'La redacción del cuento es perfecta, es decir, no es nada confusa y resulta fácil de entender. El cuento contiene e implementa de manera eficaz una variedad de elementos lingüísticos que embellecen el lenguaje.',
+        'good': 'Se observa una buena redacción con buen uso de elementos lingüísticos. Sin embargo, el cuento está algo enredado, con un hilo narrativo que se rompe de vez en cuando.',
+        'excellent': 'La redacción del cuento es perfecta, es decir, no es nada confusa y resulta fácil de entender, a pesar de (posiblemente) incorporar estructuras narrativas complejas. El cuento contiene e implementa de manera eficaz una variedad de elementos lingüísticos que embellecen el lenguaje.',
     }, 
     {
         criteria: 'Formato',
-        'bad': 'El formato es pobre o inexistente. Aparece alguna indicación que revele la identidad del autor.',
-        'normal': 'El formato cumple los mínimos necesarios.',
-        'good': 'El formato presenta imperfecciones menores.',
-        'excellent': 'Sigue el formato APA a la perfección y cumple con todos los requerimientos estructurales expuestos en la convocatoria.'
+        'bad': 'El formato es pobre o inexistente. El cuento resulta imposible o muy difícil de leer. Existe algún archivo, plantilla, logo externo que empobrezca la presentación. El o la estudiante rompe el anonimato, al mencionar o dar indicio de: nombre, profesor o profesora, grupo, programa, matrícula, etc.',
+        'normal': 'El formato presenta errores graves. El cuerpo del documento rebasa por mucho las seis páginas o es un microcuento de unos cuantos párrafos. La tipografía hace difícil la lectura. Hay *exceso* de violaciones a las bases de la convocatoria con fines artísticos.',
+        'good': 'El formato presenta imperfecciones menores. No se cita donde es necesario citar. Los márgenes distan mucho de una pulgada. El interlineado dista mucho de 1.5. El cuento se pasa por una o dos páginas de lo acordado.',
+        'excellent': 'Sigue el formato a la perfección o muy bien. Los errores son menores, como no centrar el título, no justificar el texto o un interlineado ligeramente distinto de 1.5 que no perjudica la comprensión'
     }
 ]
 
